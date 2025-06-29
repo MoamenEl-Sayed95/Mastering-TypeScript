0ee6749f6b752ae932ad9dadd870ab9277b24a64
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-06-29 16:44:47 +0300

    Update file

--- Types/objects.ts
+++ Types/objects.ts
@@ -26,3 +26,38 @@ function doublePoint(point: Point): Point {
 
     return { x: point.x * 2, y: point.y * 2 };
 }
+
+type Song = {
+    title: string,
+    artist: string,
+    numStreams: number,
+    credits:
+    {
+        producer: string,
+        writer: string
+    };
+};
+
+function calculatePayout(song: Song): number {
+    return song.numStreams * 0.0033;
+}
+
+function printSong(song: Song): void {
+    console.log(`${song.title} - ${song.artist}`);
+
+}
+
+
+const mySong: Song = {
+    title: "Unchained Melody",
+    artist: "Righteous Brothers",
+    numStreams: 12873321,
+    credits: {
+        producer: "Phil Spector",
+        writer: "Alex North"
+    }
+};
+
+const earnings = calculatePayout(mySong);
+console.log(earnings);
+printSong(mySong);
