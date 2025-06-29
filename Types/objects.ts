23938e6c4a42be90c888f7f81fa35409fa778482
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-06-29 15:14:17 +0300

    modified Type Aliases

--- Types/objects.ts
+++ Types/objects.ts
@@ -1,11 +1,28 @@
-function printName(person: { first: string; last: string }): void {
-    console.log(`${person.first} ${person.last}`);
-}
-printName({ first: 'Thomas', last: 'Jenkins' });
+// function printName(person: { first: string; last: string }): void {
+//     console.log(`${person.first} ${person.last}`);
+// }
+// printName({ first: 'Thomas', last: 'Jenkins' });
 
-let coordinate: { x: number; y: number } = { x: 34, y: 2 };
+// let coordinate: { x: number; y: number } = { x: 34, y: 2 };
+
+// function randomCoordinate(): { x: number, y: number } {
+//     return { x: Math.random(), y: Math.random() };
+// }
+// printName({ first: 'Mick', last: 'Jagger', age: 473 });
+// const singer = { first: 'Mick', last: 'Jagger', age: 473, isAlive: true };
+// printName(singer);
+
+type Point = {
+    x: number;
+    y: number;
+}
+let coordinate: Point = { x: 34, y: 2 };
 
 function randomCoordinate(): { x: number, y: number } {
     return { x: Math.random(), y: Math.random() };
 }
-printName({ first: 'Mick', last: 'Jagger' });
+
+function doublePoint(point: Point): Point {
+
+    return { x: point.x * 2, y: point.y * 2 };
+}
