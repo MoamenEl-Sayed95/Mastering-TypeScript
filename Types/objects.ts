ad6737563bd6b612a710b5f8e38d526803ee0ac4
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-06-29 14:53:24 +0300

    first commit

---
+++ Types/objects.ts
@@ -0,0 +1,11 @@
+function printName(person: { first: string; last: string }): void {
+    console.log(`${person.first} ${person.last}`);
+}
+printName({ first: 'Thomas', last: 'Jenkins' });
+
+let coordinate: { x: number; y: number } = { x: 34, y: 2 };
+
+function randomCoordinate(): { x: number, y: number } {
+    return { x: Math.random(), y: Math.random() };
+}
+printName({ first: 'Mick', last: 'Jagger' });
