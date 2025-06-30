233ed14a8ea81439363e2b15736d10e41763a95a
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-06-30 11:59:19 +0300

    Update tuplesEnums file

--- Types/tuplesEnums.ts
+++ Types/tuplesEnums.ts
@@ -2,14 +2,40 @@
 
 // const color: number[] = [23, 45, 234, 234]
 
-const color: [number,number,number] = [255,0,45];
+const color: [number, number, number] = [255, 0, 45];
+
+type HTTPResponse = [number, string];
+const goodRes: HTTPResponse = [200, 'ok']
+// goodRes[0] = '200'
+goodRes.push(123)
+goodRes.pop()
+goodRes.pop()
+goodRes.pop()
+
+const responses: HTTPResponse[] = [[404, 'Not Found'], [200, 'OK']]
+
+enum OrderStatus {
+    PENDING,
+    SHIPPED,
+    DELIVERD,
+    RETURNED
+}
+
+const myStatus = OrderStatus.DELIVERD;
+
+function isDelivered(status: OrderStatus) {
+    return status === OrderStatus.DELIVERD;
+}
+
+isDelivered(OrderStatus.RETURNED);
 
 
-
-
-
-
-
+enum ArrowKeys {
+    UP = 'up',
+    DOWN = 'down',
+    LEFT = 'left',
+    RIGHT = 'right',
+}
 
 
 export { };
