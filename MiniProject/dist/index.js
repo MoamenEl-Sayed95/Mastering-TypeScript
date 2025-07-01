38178cad84133d7c4f33253a57a9b2b81ef8f16f
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-01 16:46:20 +0300

    Add handleSubmit Function & Update

--- MiniProject/dist/index.js
+++ MiniProject/dist/index.js
@@ -2,10 +2,20 @@
 var btn = document.getElementById("btn");
 var input = document.getElementById("todoinput");
 var form = document.querySelector("form");
-form.addEventListener("submit", function (e) {
+var list = document.getElementById("todolist");
+function handleSubmit(e) {
     e.preventDefault();
     console.log("SUBMITTED!");
-});
+    var newTodoText = input.value;
+    var newLi = document.createElement("li");
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    newLi.append(newTodoText);
+    newLi.append(checkbox);
+    list.append(newLi);
+    input.value = "";
+}
+form.addEventListener("submit", handleSubmit);
 // btn.addEventListener("click", function () {
 //     alert(input.value);
 //     input.value = "";
--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,11 +1,23 @@
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
 const form = document.querySelector("form")!;
+const list = document.getElementById("todolist")!;
 
-form.addEventListener("submit", function (e) {
+function handleSubmit(e: SubmitEvent) {
     e.preventDefault();
     console.log("SUBMITTED!");
-})
+    const newTodoText = input.value;
+    const newLi = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    newLi.append(newTodoText);
+    newLi.append(checkbox);
+    list.append(newLi);
+    input.value = "";
+}
+
+form.addEventListener("submit", handleSubmit);
+
 // btn.addEventListener("click", function () {
 //     alert(input.value);
 //     input.value = "";
