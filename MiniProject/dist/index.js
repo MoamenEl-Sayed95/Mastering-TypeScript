bfb83a8fd3a8fa53fa93865842398f8bf49e67ad
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-01 18:02:39 +0300

    Finish MiniProject

--- MiniProject/dist/index.js
+++ MiniProject/dist/index.js
@@ -3,20 +3,39 @@ var btn = document.getElementById("btn");
 var input = document.getElementById("todoinput");
 var form = document.querySelector("form");
 var list = document.getElementById("todolist");
+var todos = readTodos();
+todos.forEach(createTodo);
+function readTodos() {
+    var todosJSON = localStorage.getItem("todos");
+    if (todosJSON === null)
+        return [];
+    return JSON.parse(todosJSON);
+}
+function saveTodos() {
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
 function handleSubmit(e) {
     e.preventDefault();
-    console.log("SUBMITTED!");
-    var newTodoText = input.value;
+    var newTodo = {
+        text: input.value,
+        completed: false,
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    saveTodos();
+    input.value = "";
+}
+function createTodo(todo) {
     var newLi = document.createElement("li");
     var checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    newLi.append(newTodoText);
+    checkbox.checked = todo.completed;
+    checkbox.addEventListener("change", function () {
+        todo.completed = checkbox.checked;
+        saveTodos();
+    });
+    newLi.append(todo.text);
     newLi.append(checkbox);
     list.append(newLi);
-    input.value = "";
 }
 form.addEventListener("submit", handleSubmit);
-// btn.addEventListener("click", function () {
-//     alert(input.value);
-//     input.value = "";
-// });
