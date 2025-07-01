3aef7ea2f7600e9bf26c9c2bfef145a7d98b8742
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-01 16:18:37 +0300

    Update Html file & add form

--- MiniProject/dist/index.js
+++ MiniProject/dist/index.js
@@ -1,7 +1,12 @@
 "use strict";
 var btn = document.getElementById("btn");
 var input = document.getElementById("todoinput");
-btn.addEventListener("click", function () {
-    alert(input.value);
-    input.value = "";
+var form = document.querySelector("form");
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log("SUBMITTED!");
 });
+// btn.addEventListener("click", function () {
+//     alert(input.value);
+//     input.value = "";
+// });
--- MiniProject/src/index.ts
+++ MiniProject/src/index.ts
@@ -1,9 +1,14 @@
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
+const form = document.querySelector("form")!;
 
-btn.addEventListener("click", function () {
-    alert(input.value);
-    input.value = "";
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    console.log("SUBMITTED!");
+})
+// btn.addEventListener("click", function () {
+//     alert(input.value);
+//     input.value = "";
 
-});
+// });
 
