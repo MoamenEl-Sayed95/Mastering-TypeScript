6d1fc99a00f628fa1816a26f872aadee2ffc7819
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-02 17:19:34 +0300

    Create typescript classes

---
+++ classesRecap/src/index.ts
@@ -0,0 +1,11 @@
+class Player {
+    first: string;
+    last: string;
+    score: number = 0;
+    constructor(first: string, last: string) {
+        this.first = first;
+        this.last = last;
+        
+    }
+}
+const elton = new Player("Elton","Steele");
