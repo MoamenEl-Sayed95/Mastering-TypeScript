e33c1086520481a8d9cebd91baa3aa64c8545111
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-02 13:53:51 +0300

    Fix Bugs

--- classesRecap/dist/index.js
+++ classesRecap/dist/index.js
@@ -1,47 +1,69 @@
-class Player{
-    #score = 0;
-    #numLives = 10;
-    constructor(first,last){
-        this.first = first;
-        this.last = last;
-        this.#secret();
+class Player {
+  #score = 0;
+  #numLives = 10;
+  constructor(first, last) {
+    this.first = first;
+    this.last = last;
+    this.#secret();
+  }
+  get fullName() {
+    return `${this.first} ${this.last}`;
+  }
+  set fullName(newName) {
+    const [first, last] = newName.split(" ");
+    this.first = first;
+    this.last = last;
+  }
+  get score() {
+    return this.#score;
+  }
+  set score(newScore) {
+    if (newScore < 0) {
+      throw new Error("Score must be positive!");
     }
-    get fullName(){
-        return `${this.first} ${this.last}`;
-    }
-    getScore(){
-        return this.#score;
-    }
-    updateScore(newScore){
-        this.#score = newScore;
-    }
-    taunt(){
-        console.log("BOOYAH!");
-    }
-    loseLife(){
-        this.#numLives -=1;
-    }
-    #secret(){
-        console.log("SECRET!!!");
-    }
+    this.#score = newScore;
+  }
+
+  updateScore(newScore) {
+    this.#score = newScore;
+  }
+  taunt() {
+    console.log("BOOYAH!");
+  }
+  loseLife() {
+    this.#numLives -= 1;
+  }
+  #secret() {
+    console.log("SECRET!!!");
+  }
 }
 
-const palyer1 = new Player("blue","steele");
-// palyer1.taunt();
-// console.log(palyer1.first);
-// console.log(palyer1.last);
-// console.log(palyer1);
+const player1 = new Player("blue", "steele");
+// player1.taunt();
+// console.log(player1.first);
+// console.log(player1.last);
+// console.log(player1);
 
-// console.log(palyer1.numLives);
-// palyer1.loseLife();
-// console.log(palyer1.numLives);
-// palyer1._score = -281731298;
+// console.log(player1.numLives);
+// player1.loseLife();
+// console.log(player1.numLives);
+// player1._score = -281731298;
 
-console.log(palyer1.getScore());
-console.log(palyer1.updateScore(28));
-console.log(palyer1.getScore());
+// console.log(player1.getScore());
+// console.log(player1.updateScore(28));
+// console.log(player1.getScore());
 
-console.log(palyer1.fullName);
+// console.log(player1.score);
+// player1.score = 324;
+// console.log(player1.score);
 
-// const palyer2 = new Player("charlie","brown");
-// palyer2.taunt();
+// const player1 = new Player("charlie","brown");
+// player1.taunt();
+
+// player1.score;
+// player1.score = 213987;
+// player1.fullName;
+
+console.log(player1.fullName);
+player1.fullName = "Amy Adams";
+console.log(player1);
