d0e2a74808ca6381864ac7a2430d43b870c35383
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-04 14:51:37 +0300

    Add getters & setters

--- classesRecap/dist/index.js
+++ classesRecap/dist/index.js
@@ -12,14 +12,27 @@
 //     }
 // }
 class Player {
-    // private score: number = 0;
-    constructor(first, last, score) {
+    constructor(first, last, _score) {
         this.first = first;
         this.last = last;
-        this.score = score;
+        this._score = _score;
     }
     secretMethod() {
         console.log("SECRET METHOD!!");
     }
+    get fullName() {
+        return `${this.first} ${this.last}`;
+    }
+    get score() {
+        return this._score;
+    }
+    set score(newScore) {
+        if (newScore < 0) {
+            throw new Error("Score can't be negative!");
+        }
+        this._score = newScore;
+    }
 }
-const elton = new Player("Elton", "Steele");
+const elton = new Player("Elton", "Steele", 100);
+elton.fullName;
+elton.score = 99;
--- classesRecap/src/index.ts
+++ classesRecap/src/index.ts
@@ -14,16 +14,26 @@
 // }
 
 class Player {
-
-    // private score: number = 0;
-
-    constructor(public first: string, public last: string, private score: number) {
+    constructor(public first: string, public last: string, private _score: number) {
 
     }
     private secretMethod(): void {
         console.log("SECRET METHOD!!");
     }
-
+    get fullName(): string {
+        return `${this.first} ${this.last}`;
+    }
+    get score():number{
+        return this._score;
+    }
+    set score(newScore:number){
+        if(newScore < 0){
+            throw new Error("Score can't be negative!");
+        }
+        this._score = newScore;
+    }
 }
 
-const elton = new Player("Elton", "Steele");
+const elton = new Player("Elton", "Steele", 100);
+elton.fullName;
+elton.score = 99;
