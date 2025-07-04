fa73715c9d4b43797bee08d3bc51dd3848a38b67
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-04 15:14:53 +0300

    Add Classes & Interfaces

--- classesRecap/src/index.ts
+++ classesRecap/src/index.ts
@@ -14,27 +14,27 @@
 // }
 
 class Player {
-    constructor(public first: string, public last: string, protected _score: number) {}
+    constructor(public first: string, public last: string, protected _score: number) { }
     private secretMethod(): void {
         console.log("SECRET METHOD!!");
     }
     get fullName(): string {
         return `${this.first} ${this.last}`;
     }
-    get score():number{
+    get score(): number {
         return this._score;
     }
-    set score(newScore:number){
-        if(newScore < 0){
+    set score(newScore: number) {
+        if (newScore < 0) {
             throw new Error("Score can't be negative!");
         }
         this._score = newScore;
     }
 }
 
-class SuperPlayer extends Player{
+class SuperPlayer extends Player {
     public isAdmin: boolean = true;
-    maxScore(){
+    maxScore() {
         this._score = 999999;
     }
 }
@@ -43,3 +43,30 @@ class SuperPlayer extends Player{
 const elton = new Player("Elton", "Steele", 100);
 elton.fullName;
 elton.score = "23";
+
+interface Colorful {
+    color: string;
+}
+
+interface Printable {
+    print(): void;
+}
+
+class Bike implements Colorful {
+    constructor(public color: string) {
+    }
+}
+
+
+class Jacket implements Colorful , Printable{
+    constructor(public brand: string, public color: string) {
+
+    }
+    print(){
+        console.log(`${this.color} ${this.brand} jacket`);
+    }
+    
+}
+
+const bike1 = new Bike("red");
+const jacket1 = new Jacket("Prada", "black");
