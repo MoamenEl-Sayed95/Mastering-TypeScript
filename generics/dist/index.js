8b1106ab08873273db19c0c80fa009c3480519fa
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-04 23:40:09 +0300

    Writing another generic function

--- generics/dist/index.js
+++ generics/dist/index.js
@@ -1,4 +1,32 @@
 "use strict";
-// const nums: number[] = [];
-var nums = [];
-var colors = [];
+// // const nums: number[] = [];
+// const nums: Array<number> = [];
+// const colors: Array<string> = [];
+// const inputEl = document.querySelector<HTMLInputElement>("#username")!;
+// console.dir(inputEl);
+// inputEl.value = "Hacked!";
+// const btn = document.querySelector<HTMLButtonElement>(".btn")!;
+function numberIdentity(item) {
+    return item;
+}
+function stringIdentity(item) {
+    return item;
+}
+function booleanIdentity(item) {
+    return item;
+}
+function identity(item) {
+    return item;
+}
+// identity<number>(7);
+// identity<string>("hello");
+// identity<Cat>()
+function getRandomElement(list) {
+    var randIxd = Math.floor(Math.random() * list.length);
+    return list[randIxd];
+}
+console.log(getRandomElement(["a", "b", "c"]));
+getRandomElement([5, 6, 21, 354, 567, 234, 654]);
+// [4, 5, 6, 7]
+// [true, false, true]
+// [{}, {}, {}]
--- generics/src/index.ts
+++ generics/src/index.ts
@@ -1,3 +1,48 @@
-// const nums: number[] = [];
-const nums: Array<number> = [];
-const colors: Array<string> = [];
+// // const nums: number[] = [];
+// const nums: Array<number> = [];
+// const colors: Array<string> = [];
+
+// const inputEl = document.querySelector<HTMLInputElement>("#username")!;
+// console.dir(inputEl);
+// inputEl.value = "Hacked!";
+
+// const btn = document.querySelector<HTMLButtonElement>(".btn")!;
+
+function numberIdentity(item: number): number {
+    return item;
+}
+
+function stringIdentity(item: string): string {
+    return item;
+}
+
+function booleanIdentity(item: boolean): boolean {
+    return item;
+}
+
+// function identity(item:any):any{
+//     return item;
+// }
+interface Cat {
+    name: string,
+    breed: string
+}
+
+function identity<T>(item: T): T {
+    return item;
+}
+
+// identity<number>(7);
+// identity<string>("hello");
+// identity<Cat>()
+
+function getRandomElement<T>(list:T []):T {
+    const randIxd = Math.floor(Math.random() * list.length);
+    return list[randIxd];
+}
+
+console.log(getRandomElement<string>(["a","b","c"]));
+getRandomElement<number>([5,6,21,354,567,234,654])
+// [4, 5, 6, 7]
+// [true, false, true]
+// [{}, {}, {}]
