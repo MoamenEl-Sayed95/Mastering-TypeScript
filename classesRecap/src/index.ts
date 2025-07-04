e399a186c8b5dee3c35dfc10e940f862557471bf
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-04 16:25:41 +0300

    Creating Abstract Classes

--- classesRecap/src/index.ts
+++ classesRecap/src/index.ts
@@ -58,15 +58,50 @@ class Bike implements Colorful {
 }
 
 
-class Jacket implements Colorful , Printable{
+class Jacket implements Colorful, Printable {
     constructor(public brand: string, public color: string) {
 
     }
-    print(){
+    print() {
         console.log(`${this.color} ${this.brand} jacket`);
     }
-    
+
 }
 
 const bike1 = new Bike("red");
 const jacket1 = new Jacket("Prada", "black");
+
+
+
+abstract class Employee {
+    constructor(public first: string, public last: string) { }
+    abstract getPay(): number;
+    greet() {
+        console.log("HELLO!");
+    }
+}
+
+class FullTimeEmployee extends Employee {
+    constructor(first: string, last: string, private salary: number) {
+        super(first, last);
+
+    }
+    getPay(): number {
+        return this.salary;
+    }
+}
+
+class PartTimeEmployee extends Employee {
+    constructor(first: string, last: string, private hourlyRate: number, private hoursWorked: number) {
+        super(first, last);
+    }
+    getPay(): number {
+        return this.hourlyRate * this.hoursWorked;
+    }
+}
+
+const betty = new FullTimeEmployee("Betty", "White", 95000);
+console.log(betty.getPay());
+
+const bill = new PartTimeEmployee("Bill", "Billerson", 24, 1100);
+console.log(bill.getPay());
