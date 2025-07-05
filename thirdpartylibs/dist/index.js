40ac4005a8aaccf692f3e75a7ced4d1143830670
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-05 04:37:15 +0300

    Working with axios types

--- thirdpartylibs/dist/index.js
+++ thirdpartylibs/dist/index.js
@@ -1,3 +1,23 @@
 "use strict";
-console;
-document.getElementById();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var axios_1 = __importDefault(require("axios"));
+// axios.get<User>("https://jsonplaceholder.typicode.com/users/1").then((res) => {
+//     console.log("WOO!!!");
+//     printUser(res.data);
+// }).catch(e => {
+//     console.log("ERROR!", e);
+// });
+axios_1.default.get("https://jsonplaceholder.typicode.com/users").then(function (res) {
+    console.log("WOO!!!");
+    res.data.forEach(printUser);
+}).catch(function (e) {
+    console.log("ERROR!", e);
+});
+function printUser(user) {
+    console.log(user.name);
+    console.log(user.email);
+    console.log(user.phone);
+}
