3ce5d19bb18b09bccef5fee9ade988b9a70196c4
MoamenEl-Sayed95 <moamenprogrammer95@gmail.com>
2025-07-05 16:03:40 +0300

    Add Modules

---
+++ modules/src/index.ts
@@ -0,0 +1,4 @@
+import { add, sample } from "./utils.js";
+
+console.log(sample([12, 3, 34]));
+console.log(add(1, 2));
--- thirdpartylibs/src/index.ts
+++ thirdpartylibs/src/index.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 import _ from "lodash";
 
-
-
 interface User {
     id: number;
     name: string;
